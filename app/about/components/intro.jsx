bdd40af42ddaa5e5d9dfccb2fbf4997df06d5e3f
Prateek Singh <prateeksingh9741@gmail.com>
2023-09-18 13:56:36 +0530

    Initial setup
    Signed-off-by: Prateek Singh <prateeksingh9741@gmail.com>

---
+++ app/about/components/intro.jsx
@@ -0,0 +1,28 @@
+export default function Intro() {
+  return (
+    <div className="max-h-full text-clip overflow-hidden">
+      <div className="text-9xl">
+        <div className="">
+          <h1>Hey there !</h1>
+          <h1> I am Prateek, a software Developer from India.</h1>
+        </div>
+        <p>
+          A software engineer, with a passion for innovation. My current
+          experience is with Web and Cloud Native Technologies, but I like
+          exploring. How ? my journey below will give a hint !
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          facere numquam voluptatum repudiandae dignissimos, earum molestiae
+          dicta ducimus voluptate velit non eius saepe. Commodi sint sed facere
+          enim officia in eum, ad natus, impedit exercitationem quasi at soluta
+          modi quas obcaecati laborum totam quo corrupti tempore tempora ipsa
+          aut. In nulla modi recusandae beatae, amet nobis iure ex saepe
+          veritatis autem nihil repellat laboriosam, debitis similique. Dolores
+          nemo consequuntur ullam delectus. Incidunt sint alias voluptate modi
+          et, ducimus obcaecati hic.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ app/about/page.jsx
@@ -0,0 +1,9 @@
+import Intro from './components/intro';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <Intro />
+    </div>
+  );
+}
